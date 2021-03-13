91d5bdaf3d65016a23c4142a869c1c255b7a34c0
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-03-13 16:56:59 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Github Explorer
+
+### Howdy! Welcome to Github Explorer!
+
+This is a simples React project and yes, I'm redoing it. 
+I've done it some months ago, as a part of Rocketseat's Bootcamp (GoStack).
+
+Now, March of 2021, I'm updating some concepts and learning again!
+
+This project works like a Create React App, but in a simpler way, more
+"atomicaly". Both W were manually configurated.
+
+Well, that's all folks! Enjoy!
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+export function RespositoryList() {
+  return (
+    <section className="repository-list" >
+      <h1>Repository List</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">Access Repository</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">Access Repository</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">Access Repository</a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+
+          <a href="">Access Repository</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
