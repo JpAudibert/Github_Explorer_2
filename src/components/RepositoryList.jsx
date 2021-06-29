f7269a8096c15fc3c4a4069ce2b96fd7ff368462
João Pedro Basso Audibert <hotsjoao@gmail.com>
2021-06-28 21:28:16 -0300

    adding dynamic listing

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,25 @@
+import { useEffect, useState } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
 import '../styles/repositories.scss';
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform',
-}
-
 export function RespositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.github.com/orgs/rocketseat/repos`)
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, [])
+
+  console.log(repositories)
+
   return (
     <section className="repository-list" >
       <h1>Repository List</h1>
 
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository}/>)}  
       </ul>
     </section>
   )
